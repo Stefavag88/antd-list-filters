6e0d05355ee651b5edcdaf7096bb2fa66d2c0e77
Vaggelis Stefanakis <stefanakisvag@gmail.com>
2018-12-29 16:02:51 +0200

    Replace webpack with rollup . Working builds

---
+++ src/lib/Components/NumberFilter/index.js
@@ -0,0 +1,76 @@
+import React  from 'react';
+import { InputNumber, Select } from 'antd';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const Option = Select.Option;
+
+class NumberFilter extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            operator: "==",
+            number: null
+        }
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        this.props.onChange(this.state)
+    }
+
+    handleChange = (value) => {
+
+        if (typeof value === 'string') {
+            this.setState((state, props) => {
+                return { operator: value }
+            });
+        } else {
+            this.setState((state, props) => {
+                return { number: value }
+            });
+        }
+
+    }
+
+    render() {
+
+        const { name, min, max } = this.props;
+
+        return (
+            <div className="ant-filter-container">
+                <span>{name}</span>
+                <div className="filter-content">
+                    <Select
+                        className="operator-value"
+                        defaultValue="=="
+                        onChange={this.handleChange}>
+                        <Option value=">=">{' >= '}</Option>
+                        <Option value="<=">{' <= '}</Option>
+                        <Option value=">">{' > '}</Option>
+                        <Option value="<">{' < '} </Option>
+                        <Option value="==">{' = '}</Option>
+                        <Option value="!=">{' != '}</Option>
+                    </Select>
+                    <InputNumber
+                        className="filter-value"
+                        onChange={this.handleChange}
+                        key={name}
+                        placeholder={name}
+                        min={min}
+                        max={max} />
+                </div>
+            </div>
+        )
+    }
+}
+
+NumberFilter.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+    min: PropTypes.number,
+    max: PropTypes.number
+}
+
+export default NumberFilter;
---
+++ src/lib/Components/StringInputFilter/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Input } from 'antd';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+class StringInputFilter extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: null
+        }
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        this.props.onChange(this.state);
+    }
+
+    handleChange = (event) => {
+
+        const {value} = event.target; 
+
+        this.setState((state, props) => {
+            return { value }
+        });
+    }
+
+    render() {
+
+        const { name } = this.props;
+
+        return (
+            <div className="ant-filter-container">
+                <span>{name}</span>
+                <div className="filter-content">
+                <Input
+                    style={{width:'100%', minWidth:200}}
+                    key={name}
+                    onChange={this.handleChange}
+                    placeholder={name} />
+                </div> 
+            </div>
+        )
+    }
+}
+
+StringInputFilter.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired
+}
+
+export default StringInputFilter;
